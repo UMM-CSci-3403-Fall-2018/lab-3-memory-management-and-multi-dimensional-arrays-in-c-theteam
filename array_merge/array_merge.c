27184c04f0a3b635c8622fd8a5982aead624c34f
rabex028 <rabex028@morris.umn.edu>
2018-09-27 11:23:50 -0500

    Begin writing top level function in array_merge
    Added a scaffold for future work in array_merge
    consisting of nested for loops. Started writing
    a helper function to return the total number of
    elements in an array. Tests are failing.

---
+++ array_merge/array_merge.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+#include "array_merge.h"
+#include "mergesort.h"
+
+int* array_merge(int num_arrays, int* sizes, int** values) {
+//	int* unique_values = (int*) malloc( 
+
+	for(int i = 0; i < num_arrays, i++) {
+	
+		for(int j = 0; j < sizes[i]; j++){
+
+	}
+}
+}
+
+int total_elements(int* sizes) {
+	int total_elements = 0;
+
+	for(int i = 0; i < s
+}
+
