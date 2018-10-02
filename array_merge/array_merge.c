4dbd67dea2a97a9dde15a71f63ff5f20ba037155
rabex028 <rabex028@morris.umn.edu>
2018-10-02 11:22:05 -0500

    array_merge creates array with merged elements
    Added functionality for counting the total
    number of elements inside of the multidimensional
    array. Also added a function that checks if an
    array containing unique values of the multidimensional
    array already contains a particular element. Program
    calls mergesortRange on array of merged values to be
    returned. Most tests are failing

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,22 +1,60 @@
 #include <stdlib.h>
 
 #include "array_merge.h"
-#include "mergesort.h"
+#include "../mergesort/mergesort.h"
+#include "../mergesort/mergesort.c"
 
-int* array_merge(int num_arrays, int* sizes, int** values) {
-//	int* unique_values = (int*) malloc( 
 
-	for(int i = 0; i < num_arrays, i++) {
+int total_elements(int num_arrays, int* sizes) {
+        int total_elements = 0;
+
+        for(int i = 0; i < num_arrays; i++) {
+        total_elements += sizes[i];
+        }
+	return total_elements;
+}
+
+bool contains_element(int element, int size, int* array) {
+        bool contains = false;
+
+        for (int i = 0; i < size; i++){
+
+                if (array[i] == element){
+                contains = true;
+                }
+        }
+        return contains;
+}
+
+
+
+int* array_merge(int num_arrays, int* sizes, int** values) { 
+	int max_size = total_elements(num_arrays,sizes);
+	int unique_count = 0;
+
+	int* unique_values = (int*) malloc(max_size * sizeof(int));
 	
+	for(int i = 0; i < num_arrays; i++){
+		
 		for(int j = 0; j < sizes[i]; j++){
-
+			if (!contains_element(values[i][j], max_size, values[i])){
+			unique_values[unique_count] = values[i][j];
+			unique_count++;
+			}
+		
+		}
+	
 	}
-}
-}
 
-int total_elements(int* sizes) {
-	int total_elements = 0;
+	int* merged_elements = (int*) malloc(unique_count + 1 * sizeof(int));
 
-	for(int i = 0; i < s
+	merged_elements[0] = unique_count;
+
+	for(int i = 1; i <= unique_count; i++) {
+	merged_elements[i] = unique_values[i - 1];
+	}
+
+	mergesortRange(merged_elements, 1, unique_count + 1);
+	
+	return merged_elements;
 }
-
